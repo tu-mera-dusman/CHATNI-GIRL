d716eb3c761f6b054806d6b4039f72f1d6ad75c8
tu-mera-dusman <ramuseth41@gmail.com>
2025-02-13 04:10:39 +0530

    Add files via upload

---
+++ CHATNI/cplugin/utils/inline.py
@@ -0,0 +1,17 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+buttons = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(text="▷", callback_data="resume_cb"),
+            InlineKeyboardButton(text="II", callback_data="pause_cb"),
+            InlineKeyboardButton(text="↻", callback_data="replay_cb"),
+            InlineKeyboardButton(text="‣‣I", callback_data="skip_cb"),
+            InlineKeyboardButton(text="▢", callback_data="end_cb"),
+        ]
+    ]
+)
+
+close_key = InlineKeyboardMarkup(
+    [[InlineKeyboardButton(text="✯ ᴄʟᴏsᴇ ✯", callback_data="close")]]
+)
