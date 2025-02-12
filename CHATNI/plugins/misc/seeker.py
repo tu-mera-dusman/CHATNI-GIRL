a3804cd1d0507c503dfc59c6d9514206d8daaadb
tu-mera-dusman <ramuseth41@gmail.com>
2025-02-13 05:01:02 +0530

    Update seeker.py

--- CHATNI/plugins/misc/seeker.py
+++ CHATNI/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from PROMUSIC.misc import db
-from PROMUSIC.utils.database import get_active_chats, is_music_playing
+from CHATNI.misc import db
+from CHATNI.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
