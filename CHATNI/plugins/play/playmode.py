144d136ccaeb3dcc3ab822b8451374c8581f8310
tu-mera-dusman <ramuseth41@gmail.com>
2025-02-13 05:03:47 +0530

    Update playmode.py

--- CHATNI/plugins/play/playmode.py
+++ CHATNI/plugins/play/playmode.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from PROMUSIC import app
-from PROMUSIC.utils.database import get_playmode, get_playtype, is_nonadmin_chat
-from PROMUSIC.utils.decorators import language
-from PROMUSIC.utils.inline.settings import playmode_users_markup
+from CHATNI import app
+from CHATNI.utils.database import get_playmode, get_playtype, is_nonadmin_chat
+from CHATNI.utils.decorators import language
+from CHATNI.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
 
 
