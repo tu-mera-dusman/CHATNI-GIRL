f124ae22b89ae63da05cf80101fbf5eae5b0adda
tu-mera-dusman <ramuseth41@gmail.com>
2025-02-13 04:51:45 +0530

    Update Youtube.py

--- CHATNI/platforms/Youtube.py
+++ CHATNI/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from PROMUSIC.utils.database import is_on_off
-from PROMUSIC.utils.formatters import time_to_seconds
+from CHATNI.utils.database import is_on_off
+from CHATNI.utils.formatters import time_to_seconds
 
 async def shell_cmd(cmd):
     proc = await asyncio.create_subprocess_shell(
@@ -26,7 +26,7 @@ async def shell_cmd(cmd):
     return out.decode("utf-8")
 
 
-cookies_file = "PROMUSIC/assets/cookies.txt"
+cookies_file = "CHATNI/assets/cookies.txt"
 
 class YouTubeAPI:
     def __init__(self):
