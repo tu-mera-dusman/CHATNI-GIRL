93aa159d6753f53bddfc6d6debe414271f2c03b5
tu-mera-dusman <ramuseth41@gmail.com>
2025-02-13 05:33:42 +0530

    Update start.py

--- CHATNI/utils/inline/start.py
+++ CHATNI/utils/inline/start.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 import config
 import asyncio
-from PROMUSIC import app
+from CHATNI import app
 
 
 # Start panel for inline buttons
