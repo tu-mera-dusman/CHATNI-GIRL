4139efd23570cbc9d7a71ac5a3ffe424360a23f3
tu-mera-dusman <ramuseth41@gmail.com>
2025-02-13 06:44:50 +0530

    Update Telegram.py

--- CHATNI/platforms/Telegram.py
+++ CHATNI/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from PROMUSIC import app
-from PROMUSIC.utils.formatters import (
+from CHATNI import app
+from CHATNI.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
