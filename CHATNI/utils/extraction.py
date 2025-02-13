17983aa78dec16434d673faa6b8f12c3d5ee39da
tu-mera-dusman <ramuseth41@gmail.com>
2025-02-13 05:38:46 +0530

    Update extraction.py

--- CHATNI/utils/extraction.py
+++ CHATNI/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from PROMUSIC import app
+from CHATNI import app
 
 
 async def extract_user(m: Message) -> User:
