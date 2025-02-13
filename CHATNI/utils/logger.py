9e3be884306aadaa47579b6f80a108840fe1c6b0
tu-mera-dusman <ramuseth41@gmail.com>
2025-02-13 05:39:44 +0530

    Update logger.py

--- CHATNI/utils/logger.py
+++ CHATNI/utils/logger.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import ParseMode
 
-from PROMUSIC import app
-from PROMUSIC.utils.database import is_on_off
+from CHATNI import app
+from CHATNI.utils.database import is_on_off
 from config import LOGGER_ID
 
 
